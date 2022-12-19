c28220c001b1740fdbb1e339897e997c9e5844c6
Bakterio <toman-lukas@seznam.cz>
2022-12-19 10:22:27 +0100

    418 - teapot

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,6 +3,19 @@ class RegistrationController < ApplicationController
     @user = User.new
   end
 
-  def edit
+  def create
+    @user = User.new(user_params)
+    puts @user.email
+    if @user.save
+      redirect_to root_path, notice: "Successfully singed up"
+    else
+      redirect_to sing_up_path, notice: "Something went wrong, please, check your input", status: 418 # teapot hehe
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get "sing_up", to: "registration#new"
-  post "sing_up", to: "refistration#create"
   root 'main#index'
+
   get 'main/about_us'
   get 'main/our_services'
+
+  get "sing_up", to: "registration#new"
+  post "sing_up", to: "registration#create"
 end

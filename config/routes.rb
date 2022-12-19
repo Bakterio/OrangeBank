2774eeb87bacc9a37df39736691a9807ef618377
Bakterio <toman-lukas@seznam.cz>
2022-12-19 09:47:58 +0100

    registration_controller, sing_up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get "sing_up", to: "registration#new"
+  post "sing_up", to: "refistration#create"
   root 'main#index'
   get 'main/about_us'
   get 'main/our_services'
-  resources :users
 end
---
+++ test/controllers/registration_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class RegistrationControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get registration_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get registration_edit_url
+    assert_response :success
+  end
+end

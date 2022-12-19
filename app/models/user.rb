78bf3f120c8fc19bc2812cdc630815e6252d8777
Bakterio <toman-lukas@seznam.cz>
2022-12-19 09:23:26 +0100

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password # external gem, checks for password match, hashs the password
+
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "not an email adress" }# before is user saved
+    validates :password_digest, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_172601) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_18_174805) do
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.string "password_digest", null: false
   end
 
 end

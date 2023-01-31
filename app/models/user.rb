902e2bdfd55cb11a67569312a3a934ce872baafd
Bakterio <toman-lukas@seznam.cz>
2023-01-31 19:06:39 +0100

    user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
     has_secure_password # external gem, checks for password match, hashes the password
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "not an email address" }# before is user saved
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @u = users(:deep_purple)
+    @user = User.new(email: "praha@bratislava.sk", first_name: @u.first_name, last_name: @u.last_name, password_digest: @u.password_digest)
+  end
+
+  test "should create user" do
+    assert_difference("User.count") do
+      assert @user.save!
+    end
+  end
+
+  test "should destroy user" do
+    assert @user.save!
+    assert_difference("User.count", -1) do
+      @user.destroy!
+    end
+  end
 end
